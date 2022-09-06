4703e3db7a49175b8e94c9a9b88df9168eed9d67
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-09-06 09:13:42 -0700

    commit tree abe02fdc376a6701364f4e44192366da92220d44

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
+  private
+
+  def render_invalid(invalid)
+    render json: {errors: ["validation errors"] }, status: :unprocessable_entity
+  end
 end
